916ed6875a87a919739cc7e977121712df706a1c
Evgeniy Zaitsev <zaicev-e@bk.ru>
2020-07-06 16:58:55 +0300

    tasks 8 and 9

---
+++ Figures/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Figures
+{
+    public interface IFigureMath
+    {
+        double GetSquare();
+    }
+
+    public class Treangle : IFigureMath
+    {
+        public double X_1
+        { set; get; }
+
+        public double X_2
+        { set; get; }
+
+        public double X_3
+        { set; get; }
+
+        public double Y_1
+        { set; get; }
+
+        public double Y_2
+        { set; get; }
+
+        public double Y_3
+        { set;get; }
+
+        public Treangle()
+        { }
+        public Treangle(double x1, double x2, double x3, double y1, double y2, double y3)
+        {
+            X_1 = x1;
+            X_2 = x2;
+            X_3 = x3;
+            Y_1 = y1;
+            Y_2 = y2;
+            Y_3 = y3;
+        }
+
+        public double GetSquare()
+        {
+            double res = Math.Abs(0.5*((X_1 - X_3) * (Y_2 - Y_3) - (X_2 - X_3) * (Y_1 - Y_3)));
+            return res;
+        }
+
+        public bool IsRect()
+        {
+            double p1, p2, p3, d1, d2, d3;
+            
+            p1 = Math.Sqrt((X_2 - X_1) * (X_2 - X_1) + (Y_2 - Y_1) * (Y_2 - Y_1));
+            p2 = Math.Sqrt((X_3 - X_1) * (X_3 - X_1) + (Y_3 - Y_1) * (Y_3 - Y_1));
+            p3 = Math.Sqrt((X_3 - X_2) * (X_3 - X_2) + (Y_3 - Y_2) * (Y_3 - Y_2));
+            
+            d1 = p1 * p1;
+            d2 = p2 * p2;
+            d3 = p3 * p3;
+            
+            if ((d1 - d2 + d3 < 0.0001) || (d2 - d1 + d3 < 0.0001) || (d3 - d1 + d2 < 0.0001))
+                return true;
+            else
+                return false;
+            
+        }
+
+    }
+
+    public class Circle: IFigureMath
+    {
+        public double R
+        { set; get; }
+
+        public Circle()
+        { }
+
+        public Circle(double r)
+        {
+            R = r;
+        }
+
+        public double GetSquare()
+        {
+            double res = Math.PI * R * R;
+            return res;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Treangle tr1 = new Treangle(x1:1, x2:3, x3:3, y1:1, y2:3, y3:-1);
+            Console.WriteLine("S - {0}", tr1.GetSquare());
+            Console.WriteLine("Is Rectangle - {0}", tr1.IsRect().ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FiguresTests/UnitTest1.cs
@@ -0,0 +1,58 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Figures;
+
+namespace FiguresTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        double[,] coords = new double[3, 2] {
+                { 0.5, 0.5 },
+                { 1.3, 1.3 },
+                { 1.3, 0.5 }
+            };
+        [TestMethod]
+        public void TestArea()
+        {
+            double result = Math.Abs(0.5 * ((coords[0, 0] - coords[2, 0]) * (coords[1, 1] - coords[2, 1]) - (coords[1, 0] - coords[2, 0]) * (coords[0, 1] - coords[2,1])));
+            var tr1 = new Figures.Treangle(coords[0, 0], coords[1, 0], coords[2, 0], coords[0, 1], coords[1, 1], coords[2, 1]);
+            double resLib = tr1.GetSquare(); 
+            Assert.AreEqual(resLib, result);
+        }
+        [TestMethod]
+        public void TestRect()
+        {
+            double p1, p2, p3, d1, d2, d3;
+
+            p1 = Math.Sqrt((coords[1,0] - coords[0, 0]) * (coords[1, 0] - coords[0, 0]) + (coords[1, 1] - coords[0, 1]) * (coords[1, 1] - coords[0, 1]));
+            p2 = Math.Sqrt((coords[2, 0] - coords[0, 0]) * (coords[2, 0] - coords[0, 0]) + (coords[2, 1] - coords[0, 1]) * (coords[2, 1] - coords[0, 1]));
+            p3 = Math.Sqrt((coords[2, 0] - coords[1, 0]) * (coords[2, 0] - coords[1, 0]) + (coords[2, 1] - coords[1, 1]) * (coords[2, 1] - coords[1, 1]));
+
+            d1 = p1 * p1;
+            d2 = p2 * p2;
+            d3 = p3 * p3;
+
+            bool res;
+
+            if ((d1 - d2 + d3 < 0.0001) || (d2 - d1 + d3 < 0.0001) || (d3 - d1 + d2 < 0.0001))
+                res = true;
+            else
+                res = false;
+
+            var tr1 = new Figures.Treangle(coords[0, 0], coords[1, 0], coords[2, 0], coords[0, 1], coords[1, 1], coords[2, 1]);
+            bool resLib = tr1.IsRect();
+
+            Assert.AreEqual(resLib, res);
+
+        }
+        [TestMethod]
+        public void TestCircleArea()
+        {
+            double result = Math.PI * 5 * 5;
+            var c1 = new Figures.Circle(5);
+            double resLib = c1.GetSquare();
+            Assert.AreEqual(resLib, result);
+        }
+    }
+}
